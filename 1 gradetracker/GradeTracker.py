84033bab66c60b47946c807252551418709655ad
Suri98 <59582382+Suri98@users.noreply.github.com>
2020-01-06 18:01:09 -0500

    Update GradeTracker.py
    changed the file to hack code for class

--- 1 gradetracker/GradeTracker.py
+++ 1 gradetracker/GradeTracker.py
@@ -7,7 +7,7 @@ needs to have login to protect the student data
 import statistics as s
 
 #add constants next
-admins = {'Faculty1':'ABC123','Faculty2':'ABC123'}
+admins = {'suriyah':'Henry','Faculty2':'ABC123'}
 
 #Like the admins above is a dictionary but of students. Dictionaries use curly brackets with colons to associate keys with values. In this case, each student's first name is a key. The values are lists of grades. 
 #Lists are denoted with square brackets. Values are indexed within starting with 0 for the first one. Each value is separated by commas.
@@ -72,7 +72,7 @@ def main():
     else:
         print('Valid option not selected.') #need to cause it to reprompt
 
-login = input('User: ')
+login = input('Please enter your username: ')
 
 if login in admins:
     password = input('Password: ')
